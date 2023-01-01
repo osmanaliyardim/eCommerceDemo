55ffaed3646ff3f1f6cbbbce46be05fa8cdbb2ce
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-01-01 20:25:33 +0300

    FEATURE: RabbitMQ
    RabbitMQ implementation has been completed.

---
+++ src/BuildingBlocks/EventBus/EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,18 @@
+﻿using EventBus.AzureServiceBus;
+using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.RabbitMQ;
+
+namespace EventBus.Factory;
+
+public class EventBusFactory
+{
+    public static IEventBus Create(EventBusConfig eventBusConfig, IServiceProvider serviceProvider)
+    {
+        return eventBusConfig.EventBusType switch
+        {
+            EventBusType.AzureServiceBus => new EventBusServiceBus(eventBusConfig, serviceProvider),
+            _ => new EventBusRabbitMQ(eventBusConfig, serviceProvider) // Else use RabbitMQ
+        };
+    }
+}

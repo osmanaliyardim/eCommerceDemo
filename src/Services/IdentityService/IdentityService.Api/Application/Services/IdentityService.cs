71d1e44f2a5960c5ca6192b95433c5390a691cb4
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-01-08 16:41:29 +0300

    FEATURE: Identity Service
    Simple Identity Service has been completed.

---
+++ src/Services/IdentityService/IdentityService.Api/Application/Services/IdentityService.cs
@@ -0,0 +1,37 @@
+﻿using IdentityService.Api.Application.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityService.Api.Application.Services;
+
+public class IdentityService : IIdentityService
+{
+    public Task<LoginResponseModel> Login(LoginRequestModel requestModel)
+    {
+        // DB Process will be here. Check if user information is valid and get details
+
+        var claims = new Claim[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, requestModel.UserName),
+            new Claim(ClaimTypes.Name, "Osman Ali Yardim")
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("OsmanTopSecretSecurityKeyMustBeLongAsMuchAsPossible"));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var expiry = DateTime.Now.AddDays(10);
+
+        var token = new JwtSecurityToken(claims: claims, expires: expiry, signingCredentials: creds, notBefore: DateTime.Now);
+
+        var encodedJwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        LoginResponseModel response = new()
+        {
+            UserToken = encodedJwt,
+            UserName = requestModel.UserName
+        };
+
+        return Task.FromResult(response);
+    }
+}

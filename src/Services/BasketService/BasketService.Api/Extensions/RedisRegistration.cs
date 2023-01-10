504459b787aaab528e55b5b0d9c5ccc374e61337
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-01-10 13:13:52 +0300

    FEATURE: Basket Service
    Basket Service has been completed with Redis.

---
+++ src/Services/BasketService/BasketService.Api/Extensions/RedisRegistration.cs
@@ -0,0 +1,14 @@
+﻿using StackExchange.Redis;
+
+namespace BasketService.Api.Extensions;
+
+public static class RedisRegistration
+{
+    public static ConnectionMultiplexer ConfigureRedis(this IServiceProvider services, IConfiguration configuration)
+    {
+        var redisConf = ConfigurationOptions.Parse(configuration["RedisSettings:ConnectionString"], true);
+        redisConf.ResolveDns = true;
+
+        return ConnectionMultiplexer.Connect(redisConf);
+    }
+}

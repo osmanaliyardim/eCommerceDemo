cf8cf12ab199ad6edeac5636dac9a600e6c08f5b
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-01-18 21:11:52 +0300

    FEATURE: Checkout Service
    Checkout Service has been started to develop.

--- src/Clients/BlazorWebApp/WebApp/Program.cs
+++ src/Clients/BlazorWebApp/WebApp/Program.cs
@@ -8,4 +8,23 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddTransient<IBasketService, BasketService>();
+builder.Services.AddTransient<IIdentityService, IdentityService>();
+builder.Services.AddTransient<ICatalogService, CatalogService>();
+builder.Services.AddTransient<IOrderService, OrderService>();
+
+builder.Services.AddSingleton<AppStateManger>();
+
+builder.Services.AddScoped(sp =>
+{
+    var clientFactory = sp.GetRequiredService<IHttpClientFactory>();
+
+    return clientFactory.CreateClient("ApiGatewayHttpClient");
+});
+
+builder.Services.AddHttpClient("ApiGatewayHttpClient", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5000/";
+});
+
 await builder.Build().RunAsync();
